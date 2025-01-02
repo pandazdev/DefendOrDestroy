4cdfde2ca16a751eef6aad0ee82770c4e1ee7e0e
pandazdev <106455483+pandazdev@users.noreply.github.com>
2025-01-01 17:50:40 -0800

    Progress :)

--- src/shared/BuildFuncs.lua
+++ src/shared/BuildFuncs.lua
@@ -1,21 +1,35 @@
+local Players = game:GetService("Players")
 local build = {}
-build.GRID = 1
+build.GRID = 4.5 -- Grid size
 
 function build.Snap(hit)
-    local PosX = math.floor(hit.X / build.GRID + 0.5 * build.GRID)
-    local PosY = math.floor(hit.Y / build.GRID + 0.5 * build.GRID) + 0.5
-    local PosZ = math.floor(hit.Z / build.GRID + 0.5 * build.GRID)
+    -- Snap to grid, every 4.5 studs
+    local PosX = math.round(hit.X / build.GRID) * build.GRID
+    local PosY = math.round(hit.Y / build.GRID) * build.GRID + 2.25
+    local PosZ = math.round(hit.Z / build.GRID) * build.GRID
     return Vector3.new(PosX, PosY, PosZ)
 end
 
-function build.IsSafe(pos)
-    if pos.X < -45 or pos.X > 50 then
+function build.IsSafe(plrPos, pos, ignoreTable)
+    if (plrPos - pos).Magnitude > 25 then
+        return false
+    end
+    if pos.X < -50 or pos.X > 50 then
         return false
     end
     if pos.Z < -50 or pos.Z > 50 then
         return false
     end
     if pos.Y < 0.5 or pos.Y > 50 then
+        return false
+    end
+    local parts = game.Workspace:GetPartBoundsInBox(CFrame.new(pos), Vector3.new(4.4, 4.4, 4.4))
+    if #parts > 0 then
+        for i, part in ipairs(parts) do
+            if part ~= table.find(ignoreTable, part) then
+                return false
+            end
+        end
         return false
     end
     return true
